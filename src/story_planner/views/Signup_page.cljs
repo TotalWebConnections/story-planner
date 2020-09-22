7de23cf665bc9f11f94503849ccb0a2dd340903e
peterjewicz <peterjewicz@totalwebconnections.com>
2020-09-22 13:12:40 -0500

    client side clojure.spec

--- src/story_planner/views/Signup_page.cljs
+++ src/story_planner/views/Signup_page.cljs
@@ -1,15 +1,46 @@
 (ns story-planner.views.Signup_page
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [reagent.core :as reagent :refer [atom]]
+            [clojure.spec.alpha :as s]
             [story-planner.services.scripts.navigation :refer [navigate]]
             [cljs-http.client :as http]))
 
+; TODO this whole logic is shared on the backend as well, we should probably make this into a shareable file to write onc
+(s/def ::email (s/and string? (partial re-matches #"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,63}$")))
+(s/def ::password-not-empty not-empty)
+(s/def ::password-equal (fn [{:keys [password confirm]}]
+                           (= password confirm)))
+
+(defn validate-email [email]
+  (if (s/valid? ::email email)
+    nil
+    {:email "Email is Invalid"}))
+
+(defn validate-password-empty [password]
+  (if (s/valid? ::password-not-empty password)
+    nil
+    {:password "Password is Empty"}))
+
+(defn validate-password-equal [user]
+  (if (s/valid? ::password-equal user)
+    nil
+    {:password-confirm "Passwords Don't Match"}))
+
+(defn validate-input [user]
+  (merge (validate-email (:email user))
+         (validate-password-empty (:password user))
+         (validate-password-equal user)))
+
 (defn handle-signup [user errors]
   (reset! errors nil)
-  (go (let [response (<! (http/post "http://localhost:8080/user"
-                                 {:with-credentials? false
-                                  :form-params {:email (:email @user) :password (:password @user) :password-repeat (:confirm @user)}}))]
-        (reset! errors (js->clj (js/JSON.parse (:body response)) :keywordize-keys true)))))
+  (let [error-block (validate-input @user)]
+    (if (> (count error-block) 0)
+      (reset! errors error-block)
+      (go (let [response (<! (http/post "http://localhost:8080/user"
+                                     {:with-credentials? false
+                                      :form-params {:email (:email @user) :password (:password @user) :password-repeat (:confirm @user)}}))]
+            (reset! errors (js->clj (js/JSON.parse (:body response)) :keywordize-keys true)))))))
+
 
 (defn Signup-page []
   (let [user (atom {:email "" :password "" :confirm ""})
