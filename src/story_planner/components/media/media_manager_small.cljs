23a75d7524671d50aafe4b22aa7cefd92ddd0ea3
peterjewicz <peterjewicz@totalwebconnections.com>
2020-12-23 08:33:25 -0600

    folders working in small media manager

--- src/story_planner/components/media/media_manager_small.cljs
+++ src/story_planner/components/media/media_manager_small.cljs
@@ -1,5 +1,6 @@
 (ns story-planner.components.media.media-manager-small
-  (:require [story-planner.services.scripts.api.upload :refer [upload-image]]))
+  (:require [reagent.core :as reagent :refer [atom]]
+            [story-planner.services.scripts.api.upload :refer [upload-image]]))
 
 (defn Media-Manager-Small [active images folders on-image-select]
   (let [active-folder (atom nil)]
@@ -7,16 +8,22 @@
       [:div.MediaManagerSmall {:class (str "MediaManagerSmall--" @active)}
        [:div.MediaManagerSmall__header.standard-padding
         [:h2 "Your Images"]
-        [:p {:on-click #(reset! active false)} "x"]]
+        [:p.close {:on-click #(reset! active false)} "x"]]
        [:div.MediaManagerSmall__upload
         [:input#mediaManagerSmall {:type "file"}]
         [:button {:on-click #(upload-image "mediaManagerSmall" nil)}"upload image"]]
+       (if @active-folder
+         [:div.MediaManager__currentFolder
+          [:i.fas.fa-level-up-alt.goBack {:on-click #(reset! active-folder nil)}]
+          [:p (str "Current Folder: " @active-folder)]])
        [:div.MediaManagerSmall__imageWrapper
-        (for [folder folders]
-          [:div.MediaManagerSmall__imageWrapper-folder {:on-click #(reset! active-folder folder)
-                                                        :key (str folder "-" (rand-int 100))}
-           [:i.fas.fa-folder]
-           [:h3 folder]])
+        (if (not @active-folder)
+          (for [folder folders]
+            [:div.MediaManagerSmall__imageWrapper-folder {:on-click #(reset! active-folder folder)
+                                                          :key (str folder "-" (rand-int 100))}
+             [:i.fas.fa-folder]
+             [:h3 folder]]))
         (doall (for [image images]
-                 ^{:key (str (:url image) "-" (rand-int 100))}[:div.MediaManagerSmall__imageWrapper-image
-                                                               [:img {:src (str "https://story-planner.s3.amazonaws.com/" (:url image)) :on-click #(do (reset! active false) (on-image-select (:url image)))}]]))]])))
+                 (if (or (= (:folder image) @active-folder) (and (= @active-folder nil) (= "null" (:folder image))))
+                   ^{:key (str (:url image) "-" (rand-int 100))}[:div.MediaManagerSmall__imageWrapper-image
+                                                                 [:img {:src (str "https://story-planner.s3.amazonaws.com/" (:url image)) :on-click #(do (reset! active false) (on-image-select (:url image)))}]])))]])))
