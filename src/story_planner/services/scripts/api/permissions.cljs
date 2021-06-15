def99ee228f7993b6ea70623245e6ae9ac0bb72a
Peter Jewicz <Peterjewicz@totalwebconnections.com>
2021-06-15 07:41:33 -0500

    Fix projects not loading on auth error

--- src/story_planner/services/scripts/api/permissions.cljs
+++ src/story_planner/services/scripts/api/permissions.cljs
@@ -2,12 +2,14 @@
   (:require [clojure.core.async :refer [chan]]
             [reitit.frontend.easy :as rfe]
             [story-planner.services.state.dispatcher :refer [handle-state-change]]
+            [story-planner.services.scripts.api.websocket :as ws]
             [cljs-http.client :as http]
             [story-planner.config :refer [api]]
             [story-planner.services.state.global :refer [get-from-state]]))
 
 (defn login-failed []
   (handle-state-change {:type "set-login-error" :value "You need to be logged in to view that page"})
+  (ws/close-connection) 
   (rfe/push-state :login))
 
 (defn check-token [token]
